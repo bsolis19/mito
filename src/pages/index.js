991b9f031aaf1472d9029bc8fd83f9dfb2c48198
Ben Solis <benjairsolis19@gmail.com>
2022-05-06 16:04:53 -0700

    Add about us

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,18 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 import Hero from "../components/hero"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+	text-align: center;
+	padding-top: 45px;
+	padding-bottom: 40px;
+`
+
+const Title = styled.h2`
+	font-size: 2em;
+	margin: 0.67em 0;
+`
 
 const IndexPage = () => (
   <Layout>
@@ -27,6 +39,16 @@ const IndexPage = () => (
 		<p>Construction company</p>
 		</div>
 	</Hero>
+	<Wrapper id="about-us">
+		<h1>About Us</h1>
+		<p>
+			We are a reputable general construction service provider that delivers quality work including
+			framing, drywall, painting, plumbing, and electrical services. With almost 30 years in service
+			we have experience with various <em>Home Remodeling & Renovations</em>. We provide services to
+			all of the <em>Los Angeles County</em>. No job is too big or too small.
+		</p>
+	</Wrapper>
+	
   </Layout>
 )
 
