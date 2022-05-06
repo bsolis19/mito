8efe6ca5cb6eafc7423e7d4735d9a742e361fddc
Ben Solis <benjairsolis19@gmail.com>
2022-05-06 10:48:16 -0700

    Add hero media object

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,13 +2,23 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import * as styles from "../components/hero.css" 
 
-export default function Hero() {
+export default function Hero( { children } ) {
 	return (
+		<div
+			style={{
+				// maxHeight:`100vh`,
+				display: `inline-block`,
+			}}
+		>
 		<StaticImage
 			src="../images/pexels-tommy-mcgrath-11723568.jpg"
-			className="styles.container"
-			imgClassName="styles.hero-img"
+			style={{
+				maxHeight:`calc(100vh - 69px)`,
+			}}
+
 		/>
+		<div> { children } </div>
+		</div>
 	)
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,24 @@ import Hero from "../components/hero"
 const IndexPage = () => (
   <Layout>
     <Seo title="Mito Construction" />
-	<Hero />
+	<Hero>
+		<div
+	    		style={{
+			position: `absolute`,
+			right: `12%`,
+			bottom: `4em`,
+			left: `12%`,
+			backgroundColor: `rgb(0,0,0,.3)`,
+			padding: `2em`,
+			fontSize: `1.3em`,
+			color: `#fff`,
+			textAlign: `center`,
+		    }}
+		>
+		<h1>Mito Construction Los Angeles</h1>
+		<p>Construction company</p>
+		</div>
+	</Hero>
   </Layout>
 )
 
